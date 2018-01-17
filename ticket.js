032b80f62a100f20b6de3f78646274e304268be7
zhangkai <deathlord@163.com>
2018-01-17 19:13:21 +0800

    简化了一下抢票逻辑

--- ticket.js
+++ ticket.js
@@ -1,27 +1,16 @@
 var resultDom = document.getElementById('no_filter_ticket');
 var noTicketDom = document.getElementById('no_filter_ticket_6');
 var searchButton = document.getElementById('query_ticket');
-var enableCheck = false
-window.autoSearchTime = 3000
-searchButton.addEventListener('click', function() {
-  if (searchButton.innerText !== '停止查询') {
-    enableCheck = true
-  }
-})
 Notification.requestPermission(function (permission) {
   window.setInterval(function(){
-    
-    if (enableCheck) {
-      if (document.getElementsByClassName('btn72').length > 0) {
-        enableCheck = false
-        new Notification('有票了，快去抢', {
-          body: '抢吧'
-        });
-      }
-      if (noTicketDom.style.display !== 'none') {
-        // 如果超时或者请求频繁，则重新搜索
-        searchButton.click();
-      }
+    if (document.getElementsByClassName('btn72').length > 0) {
+      new Notification('有票了，快去抢', {
+        body: '抢吧'
+      });
+    }
+    if (noTicketDom.style.display !== 'none') {
+      // 如果超时或者请求频繁，则重新搜索
+      searchButton.click();
     }
   }, 1000);
 })
